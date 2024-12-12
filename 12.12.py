667ef77e1a9682ec93285e6f16132c7bcdd9a83c
YuZhang-Chen <C112156205@nkust.edu.tw>
2024-12-12 10:52:56 +0800

    love

--- 12.12.py
+++ 12.12.py
@@ -1,2 +1,22 @@
-print('123jqwiqqeqweqq')
-print('1313')
+import turtle
+
+def draw_heart():
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    heart = turtle.Turtle()
+    heart.color("red")
+    heart.begin_fill()
+    heart.left(140)
+    heart.forward(180)
+    heart.circle(-90, 200)
+    heart.left(120)
+    heart.circle(-90, 200)
+    heart.forward(180)
+    heart.end_fill()
+
+    heart.hideturtle()
+    window.exitonclick()
+
+if __name__ == "__main__":
+    draw_heart()
