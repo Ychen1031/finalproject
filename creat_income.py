f4bc85e0cf48d212617972edbb3af3d4d2cfa63e
Ychen1031 <C112156240@nkust.edu.tw>
2024-12-26 17:25:29 +0800

    merge

--- creat_income.py
+++ creat_income.py
@@ -3,10 +3,11 @@ import os
 
 postback_data = {'方式': 'meal', 'price': 100, '詳情': '蛋炒飯'}
 
-if '方式' in postback_data:
-    if postback_data['方式'] == 'salary':
-        print('薪資為' + str(postback_data['price']) + '元' + '\n' + '細項: ' + postback_data['詳情'])
-    elif postback_data['方式'] == 'bonus':
-        print('獎金為' + str(postback_data['price']) + '元' + '\n' + '細項: ' + postback_data['詳情'])
-    elif postback_data['方式'] == 'invest':
-        print('投資為' + str(postback_data['price']) + '元' + '\n' + '細項: ' + postback_data['詳情'])
+income_types = {
+    'salary': '薪資',
+    'bonus': '獎金',
+    'invest': '投資'
+}
+
+if '方式' in postback_data and postback_data['方式'] in income_types:
+    print(f"{income_types[postback_data['方式']]}為{postback_data['price']}元\n細項: {postback_data['詳情']}")
