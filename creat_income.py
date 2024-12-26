23b780392d389cd5eecf4f7a7995d914d4143997
Ychen1031 <C112156240@nkust.edu.tw>
2024-12-26 17:32:19 +0800

    merge

--- creat_income.py
+++ creat_income.py
@@ -1,13 +1,15 @@
-import datetime
-import os
-
 postback_data = {'方式': 'meal', 'price': 100, '詳情': '蛋炒飯'}
 
-income_types = {
-    'salary': '薪資',
-    'bonus': '獎金',
-    'invest': '投資'
+# 定義方式對應的描述
+方式描述 = {
+    'salary': '薪資為',
+    'bonus': '獎金為',
+    'invest': '投資為'
 }
 
-if '方式' in postback_data and postback_data['方式'] in income_types:
-    print(f"{income_types[postback_data['方式']]}為{postback_data['price']}元\n細項: {postback_data['詳情']}")
+# 檢查 '方式' 是否在字典中
+方式 = postback_data.get('方式')
+if 方式 in 方式描述:
+    print(f"{方式描述[方式]}{postback_data['price']}元\n細項: {postback_data['詳情']}")
+else:
+    print("未知的方式")
