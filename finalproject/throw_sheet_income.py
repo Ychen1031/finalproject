daedc8c899c5a1dc92b73f10779c992d01f2f547
Ychen1031 <C112156240@nkust.edu.tw>
2025-01-02 20:41:45 +0800

    收入修改

--- finalproject/throw_sheet_income.py
+++ finalproject/throw_sheet_income.py
@@ -1,16 +1,22 @@
 import gspread
 import datetime
 from config import getdocsgoogle
-gc = gspread.service_account(filename='token.json')
-sh = gc.open_by_url(getdocsgoogle())
+
+def get_worksheet(sh, title):
+    try:
+        return sh.worksheet(title)
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = sh.add_worksheet(title=title, rows="100", cols="20")
+        worksheet.insert_row(['日期', '方式', 'price', '詳情'], 1)
+        return worksheet
 
 def pish_data(data):
-    try:
-        today = datetime.date.today().strftime("%Y-%m-%d") + 'create收入'
-        worksheet = sh.worksheet(today)
-    except gspread.exceptions.WorksheetNotFound:
-        worksheet = sh.add_worksheet(title=today, rows="100", cols="20")
-        worksheet.insert_row(['日期', '方式', 'price', '詳情'], 1)
+    gc = gspread.service_account(filename='token.json')
+    sh = gc.open_by_url(getdocsgoogle())
+    
+    today = datetime.date.today().strftime("%Y-%m-%d") + 'create收入'
+    worksheet = get_worksheet(sh, today)
+    
     try:
         worksheet.insert_rows(data, 2)
         print('成功加入數據!')
